65480024ec68b73c7c508f4bd67cb16ae20ee858
Tim <Tim.mouse@gmail.com>
2017-04-01 07:32:42 +0800

    Section 6: Update AppMoudule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
+import { AppRoutingModule } from './app.routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -23,19 +23,7 @@ import { DashboardComponent } from './dashboard.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },{
-        path: 'dashboard',
-        component: DashboardComponent
-      },{
-        path: '',
-        redirectTo:'/dashboard',
-        pathMatch: 'full'
-      }
-      ])
+    AppRoutingModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
