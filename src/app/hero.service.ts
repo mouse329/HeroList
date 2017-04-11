a8c4a9beea8a79764cb4c1d7856ea2ef56e1e627
Tim <Tim.mouse@gmail.com>
2017-04-12 06:52:42 +0800

    create new hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,7 +11,7 @@ import { Hero } from './hero';
 
 @Injectable()
 export class HeroService{
-    private headers = new Headers({'Content-Type':'application/x-www-form-urlencoded'});
+    private headers = new Headers({'Content-Type':'application/json'});
     private heroesUrl = 'http://godnodejs.hopto.org:3000/api/users'; //URL to web api
 
     constructor (private http: Http) { }
@@ -45,9 +45,19 @@ export class HeroService{
         const url = `${this.heroesUrl}/${id}`;
         return this.http.get(url)
                    .toPromise()
-                   .then(response => response.json().Users as Hero)
+                   .then(response => response.json().Users[0] as Hero)
                    .catch(this.handleError);
     }
+
+    create(name:string):Promise<Hero>{
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({email:name,password:1234}), {headers: this.headers})
+            .toPromise()
+            .then(response => response.json().Users)
+            .catch(this.handleError);
+    }
+
+    
 
     private handleError(error: any): Promise<any> {
         console.error('An error occurred', error); //for demo purposes only
